4aa914f6d543e61376688ba32bb05074cc4d3180
8297442118 <maradanavivek146@gmail.com>
2026-06-01 21:50:10 +0530

    Minimum Number

---
+++ minNumber.java
@@ -0,0 +1,16 @@
+class minNumber{
+    public static void main(String[] args){
+        int a=39;
+        int b=12;
+        int c=23;
+        if(a>b && a>c){
+            System.out.println("A is a larger number");
+        }
+        else if(b>c){
+            System.out.println("B is a larger number");
+        }
+        else{
+            System.out.println("C is a larger number");
+        }
+    }
+}
